3e6a126030068c68bb28f0e13a36fd75eff55884
BrianCuek <brianrifqidwisaputra@gmail.com>
2023-07-06 12:32:52 +0700

    UAS brian

--- application/config/database.php
+++ application/config/database.php
@@ -117,6 +117,11 @@ $capsule->addConnection([
     'prefix' => '',
 ]);
 
+
 $capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
 $capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
 $capsule->bootEloquent();
